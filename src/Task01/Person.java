dd431a0ea24068fe4714eff916101f133aad0a79
mirohi <m_hintikka@hotmail.com>
2024-03-12 19:09:15 +0200

    task 1

--- src/Task01/Person.java
+++ src/Task01/Person.java
@@ -1,6 +1,8 @@
 package Task01;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.function.Consumer;
 
 public class Person {
     private String name, city;
@@ -13,6 +15,17 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        ArrayList<Person> p1 = new ArrayList<>();
+        ArrayList<Person> persons = new ArrayList<>();
+        persons.add(new Person("pekka", 30, "helsinki"));
+        persons.add(new Person("matti", 40, "turku"));
+        persons.add(new Person("jarkko", 35, "kuopio"));
+
+        persons.sort(Comparator.comparing(n -> n.age));
+//        Comparator<Person> comparator = (n1, n2)  -> n2.name.charAt(0) - n1.name.charAt(0);
+//        persons.sort(comparator);
+        persons.forEach(person -> System.out.println(person.age));
+        persons.removeIf(p -> p.city=="turku" || p.city=="kuopio");
+        persons.forEach(person -> System.out.println(person.name));
+
     }
 }
