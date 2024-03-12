a6c882bbee57e2917496b8abd3fe71317340a086
mirohi <m_hintikka@hotmail.com>
2024-03-12 13:15:05 +0200

    task 1

--- src/Task01/Person.java
+++ src/Task01/Person.java
@@ -1,5 +1,18 @@
 package Task01;
 
+import java.util.ArrayList;
+
 public class Person {
-    private String name;
+    private String name, city;
+    private int age;
+
+    public Person(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Person> p1 = new ArrayList<>();
+    }
 }
