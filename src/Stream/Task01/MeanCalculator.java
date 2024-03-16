dac68426e711df20a4a447d931fb34fac34e4ac7
mirohi <m_hintikka@hotmail.com>
2024-03-16 17:40:34 +0200

    Task 1 done

--- src/Stream/Task01/MeanCalculator.java
+++ src/Stream/Task01/MeanCalculator.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MeanCalculator {
-    private static int[] list = {2, 4, 1, 6, 4, 2, 9, 8, 4, 6, 8};
-    private static ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(2, 6, 2, 3, 6, 4, 3, 2, 1, 9, 8, 5, 7, 6));
+    private static final int[] list = {2, 4, 1, 6, 4, 2, 9, 8, 4, 6, 8};
+    private static final ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(2, 6, 2, 3, 6, 4, 3, 2, 1, 9, 8, 5, 7, 6));
 
     public static void main(String[] args) {
-        System.out.println(String.format("%.2f", list2.stream().mapToInt(i -> i).average().getAsDouble()));
+        System.out.printf("%.2f%n", list2.stream().mapToInt(i -> i).average().getAsDouble());
         // Tai
-        System.out.println(String.format("%.2f",Arrays.stream(list).average().getAsDouble()));
+        System.out.printf("%.2f%n", Arrays.stream(list).average().getAsDouble());
     }
 }
